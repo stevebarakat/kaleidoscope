5c357e39bb09ea9d4d86f5dc84b6fa898a8a12b9
Steve <stevebarakat@gmail.com>
2021-08-07 21:14:59 -0400

    colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Kaleidoscope from "./Kaleidoscope";
 
-function App() {
+const App = () => {
+  const [colors, setColors] = useState({
+    color1: "#FF00FF",
+    color2: "#FFFF00",
+    color3: "#00FF00",
+    color4: "#FF0000",
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* COLORS */}
+      <fieldset>
+        <label htmlFor="color1">Color1</label>
+        <input
+          id="color1"
+          type="color"
+          value={colors.color1}
+          onChange={(e) => setColors({ ...colors, color1: e.target.value })}
+        />
+        <label htmlFor="color2">Color2</label>
+        <input
+          id="color2"
+          type="color"
+          value={colors.color2}
+          onChange={(e) => setColors({ ...colors, color2: e.target.value })}
+        />
+        <label htmlFor="color3">Color3</label>
+        <input
+          id="color3"
+          type="color"
+          value={colors.color3}
+          onChange={(e) => setColors({ ...colors, color3: e.target.value })}
+        />
+        <label htmlFor="color4">Color4</label>
+        <input
+          id="color4"
+          type="color"
+          value={colors.color4}
+          onChange={(e) => setColors({ ...colors, color4: e.target.value })}
+        />
+      </fieldset>
+      <Kaleidoscope colors={colors} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Kaleidoscope.js
@@ -0,0 +1,60 @@
+import styled, { keyframes } from "styled-components";
+import "./App.css";
+
+function Kaleidoscope({ colors }) {
+  return (
+    <Things colors={colors}>
+      <Thing>
+        <Thing>
+          <Thing>
+            <Thing>
+              <Thing>
+                <Thing>
+                  <Thing>
+                    <Thing>
+                      <Thing>
+                        <Thing>
+                          <Thing>
+                            <Thing>
+                              <Thing>
+                                <Thing>
+                                  <Thing></Thing>
+                                </Thing>
+                              </Thing>
+                            </Thing>
+                          </Thing>
+                        </Thing>
+                      </Thing>
+                    </Thing>
+                  </Thing>
+                </Thing>
+              </Thing>
+            </Thing>
+          </Thing>
+        </Thing>
+      </Thing>
+    </Things>
+  );
+}
+
+const kaledescopeAnimation = keyframes` 100% {transform: rotateZ(1turn)} `;
+
+const Things = styled.div`
+  div {
+    border-color: ${(p) => p.colors.color1} ${(p) => p.colors.color2}
+      ${(p) => p.colors.color3} ${(p) => p.colors.color4};
+  }
+`;
+
+const Thing = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+  border: 10px solid transparent;
+  border-radius: 25%;
+  animation: ${kaledescopeAnimation} 10s cubic-bezier(0.35, -0.14, 0.79, 1.22)
+    infinite;
+`;
+
+export default Kaleidoscope;
